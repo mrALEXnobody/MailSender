c9204bcb68f7eb2d63eee72f758cb423c84ee0d3
mrALEXnobody <scorp_predateur@mail.ru>
2019-09-21 09:55:51 +0300

    Структура приложения

--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace MailSender
         {
             InitializeComponent();
         }
+
+        private void ExitMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+            
+        }
     }
 }
