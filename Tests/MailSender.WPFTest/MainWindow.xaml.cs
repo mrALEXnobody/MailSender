02c6c7816db974178c70b7acd459bf8bae5e6a76
mrALEXnobody <scorp_predateur@mail.ru>
2019-09-24 21:46:43 +0300

    Тестовые привязки

--- Tests/MailSender.WPFTest/MainWindow.xaml.cs
+++ Tests/MailSender.WPFTest/MainWindow.xaml.cs
@@ -16,43 +16,43 @@ namespace MailSender.WPFTest
             InitializeComponent();
         }
 
-        private void SendButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            var host = "smtp.mail.ru";
-            var port = 25;
+        //private void SendButton_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    var host = "smtp.mail.ru";
+        //    var port = 25;
 
-            var userName = UserNameEditor.Text;
-            var password = PasswordEditor.SecurePassword;
+        //    var userName = UserNameEditor.Text;
+        //    var password = PasswordEditor.SecurePassword;
 
-            var msg = "Hello World!!! " + DateTime.Now.ToString();
+        //    var msg = "Hello World!!! " + DateTime.Now.ToString();
 
-            using (var client=new SmtpClient(host, port)) 
-            {
-                client.EnableSsl = true;
-                client.Credentials = new NetworkCredential(userName, password);
+        //    using (var client=new SmtpClient(host, port)) 
+        //    {
+        //        client.EnableSsl = true;
+        //        client.Credentials = new NetworkCredential(userName, password);
 
-                using (var message=new MailMessage())
-                {
-                    message.From = new MailAddress("scorp_predateur@mail.ru", "Александр");
-                    message.To.Add(new MailAddress("fun_account@mail.ru", "ALEX"));
-                    message.Subject = "Заголовок письма от " + DateTime.Now.ToString();
-                    message.Body = msg;
-                    //message.Attachments.Add(new Attachment());
+        //        using (var message=new MailMessage())
+        //        {
+        //            message.From = new MailAddress("scorp_predateur@mail.ru", "Александр");
+        //            message.To.Add(new MailAddress("fun_account@mail.ru", "ALEX"));
+        //            message.Subject = "Заголовок письма от " + DateTime.Now.ToString();
+        //            message.Body = msg;
+        //            //message.Attachments.Add(new Attachment());
 
-                    try
-                    {
-                        client.Send(message);
-                        MessageBox.Show("Почта успешно отправлена", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+        //            try
+        //            {
+        //                client.Send(message);
+        //                MessageBox.Show("Почта успешно отправлена", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
 
-                    }
-                    catch (Exception error)
-                    {
-                        MessageBox.Show(error.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
+        //            }
+        //            catch (Exception error)
+        //            {
+        //                MessageBox.Show(error.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+        //            }
                    
-                }
-            } 
+        //        }
+        //    } 
 
-        }
+        //}
     }
 }
