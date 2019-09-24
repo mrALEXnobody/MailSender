2c691dedbe301d738d01df82a74e9788b4f83e84
Александр Гусев <scorp_predateur@mail.ru>
2019-09-24 15:15:44 +0300

    Пользовательский элемент управления

---
+++ MailSender/Controls/TabItemsSwitcher.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSender.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TabItemsSwitcher.xaml
+    /// </summary>
+    public partial class TabItemsSwitcher : UserControl
+    {
+        public event EventHandler LeftButtonClick;
+        public event EventHandler RightButtonClick;
+
+        public bool LeftButtonVisible
+        {
+            get => LeftButton.Visibility == Visibility;
+            set => LeftButton.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public bool RightButtonVisible
+        {
+            get => RightButton.Visibility == Visibility;
+            set => RightButton.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public TabItemsSwitcher()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(e.Source is Button button)) return;
+
+            switch (button.Name)
+            {
+                case "LeftButton":
+                    LeftButtonClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                case "RightButton":
+                    RightButtonClick?.Invoke(this, EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+}
--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MailSender.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,36 @@ namespace MailSender
         {
             Close();
             
+        }
+
+        private void TabItemsSwitcher_OnLeftButtonClick(object sender, EventArgs e)
+        {
+            if (!(sender is TabItemsSwitcher switcher)) return;
+
+            if (MainTabControl.SelectedIndex == 0) return;
+
+            MainTabControl.SelectedIndex--;
+
+            if (MainTabControl.SelectedIndex == 0)
+            {
+                switcher.LeftButtonVisible = false;
+            }
+        }
+
+        private void TabItemsSwitcher_OnRightButtonClick(object sender, EventArgs e)
+        {
+            if (!(sender is TabItemsSwitcher switcher)) return;
+
+            var tab_count = MainTabControl.Items.Count;
+
+            if (MainTabControl.SelectedIndex == tab_count - 1) return;
+
+            MainTabControl.SelectedIndex++;
+
+            if (MainTabControl.SelectedIndex == MainTabControl.Items.Count - 1)
+            {
+                switcher.RightButtonVisible = false;
+            }
         }
     }
 }
