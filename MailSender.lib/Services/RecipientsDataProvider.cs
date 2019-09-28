cfaf303e6a197b747328c06c57e942bd9e3279c9
mrALEXnobody <scorp_predateur@mail.ru>
2019-09-28 09:37:00 +0300

    Команда сохранения данных в базе

--- MailSender.lib/Services/RecipientsDataProvider.cs
+++ MailSender.lib/Services/RecipientsDataProvider.cs
@@ -1,6 +1,7 @@
 ﻿using MailSender.lib.Data.Linq2SQL;
 using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,24 @@ namespace MailSender.lib.Services
             _db = db;
         }
 
-        public IEnumerable<Recipient> GetAll() => _db.Recipient.ToArray();
+        public IEnumerable<Recipient> GetAll()
+        {
+            _db.Refresh(RefreshMode.OverwriteCurrentValues);
+            return _db.Recipient.ToArray();
+        } 
+
+        public int Create(Recipient recipient)
+        {
+            if (recipient is null) throw new ArgumentNullException(nameof(recipient));
+
+            _db.Recipient.InsertOnSubmit(recipient);
+            SaveChanges();
+            return recipient.Id;
+        }
+
+        public void SaveChanges()
+        {
+            _db.SubmitChanges();
+        }
     }
 }
--- MailSender/ViewModel/MainWindowViewModel.cs
+++ MailSender/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using MailSender.lib.Data.Linq2SQL;
 using MailSender.lib.Services;
 using System;
@@ -7,6 +8,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MailSender.ViewModel
 {
@@ -22,21 +24,60 @@ namespace MailSender.ViewModel
             set => Set(ref windowTitle, value);
         }
 
-        public ObservableCollection<Recipient> Recipients { get; } = new ObservableCollection<Recipient>();
+        private ObservableCollection<Recipient> _Recipients = new ObservableCollection<Recipient>();
+
+        public ObservableCollection<Recipient> Recipients
+        {
+            get => _Recipients;
+            set => Set(ref _Recipients, value);
+        }
+
+        #region SelectedRecipient : Recipient - Выбранный получатель
+
+        /// <summary>Выбранный получатель</summary>
+        private Recipient _SelectedRecipient;
+
+        /// <summary>Выбранный получатель</summary>
+        public Recipient SelectedRecipient
+        {
+            get => _SelectedRecipient;
+            set => Set(ref _SelectedRecipient, value);
+        }
+        #endregion
+
+        public ICommand RefreshDataCommand { get; }
+
+        public ICommand SaveChangesCommand { get; }
 
         public MainWindowViewModel(RecipientsDataProvider RecipientsProvider)
         {
             _RecipientsProvider = RecipientsProvider;
 
+            RefreshDataCommand = new RelayCommand(OnRefreshDataCommandExecute, CanRefreshDataCommandExecute);
+            SaveChangesCommand = new RelayCommand(OnSaveChangesCommandExecute);
+
+            //RefreshData();
+        }
+
+        private void OnSaveChangesCommandExecute()
+        {
+            _RecipientsProvider.SaveChanges();
+        }
+
+        private bool CanRefreshDataCommandExecute() => true;
+
+        private void OnRefreshDataCommandExecute()
+        {
             RefreshData();
         }
 
         private void RefreshData()
         {
-            var recipients = Recipients;
-            recipients.Clear();
+            var recipients = new ObservableCollection<Recipient>();
             foreach (var recipient in _RecipientsProvider.GetAll())
                 recipients.Add(recipient);
+            Recipients = null;
+            Recipients = recipients;
         }
     }
 }
