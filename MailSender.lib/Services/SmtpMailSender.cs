ee96554a28279477be078612a5bfe5958f411d0f
Александр Гусев <scorp_predateur@mail.ru>
2019-10-08 10:37:20 +0300

    Асинхронная последовательная/параллельная рассылка почты

--- MailSender.lib/Services/SmtpMailSender.cs
+++ MailSender.lib/Services/SmtpMailSender.cs
@@ -49,6 +49,30 @@ namespace MailSender.lib.Services
             }
         }
 
+        public async Task SendAsync(Email Message, Sender From, Recipient To)
+        {
+            using (var client = new SmtpClient(_Host, _Port) { EnableSsl = _UseSSL })
+            {
+                client.Credentials = new NetworkCredential
+                {
+                    UserName = _Login,
+                    Password = _Password
+                };
+
+                using (var message = new MailMessage())
+                {
+                    message.From = new MailAddress(From.Address, From.Name);
+                    message.To.Add(new MailAddress(To.Address, To.Name));
+                    message.Subject = Message.Subject;
+                    message.Body = Message.Body;
+
+                    //await Task.Run(() => client.Send(message));
+                    await client.SendMailAsync(message).ConfigureAwait(false);
+                    
+                }
+            }
+        }
+
         public void Send(Email Message, Sender From, IEnumerable<Recipient> To)
         {
             foreach (var recipient in To)
@@ -60,6 +84,24 @@ namespace MailSender.lib.Services
             foreach (var recipient in To)
                 ThreadPool.QueueUserWorkItem(_ => Send(Message, From, recipient));
         }
-        
+
+        public async Task SendParallelAsync(Email Message, Sender From, IEnumerable<Recipient> To)
+        {
+            //await Task
+            //    .WhenAll(To.Select(recipient => SendAsync(Message, From, recipient)))
+            //    .ConfigureAwait(false);
+
+            var send_tasks = new List<Task>();
+            foreach (var recipient in To)
+                send_tasks.Add(SendAsync(Message, From, recipient));
+
+            await Task.WhenAll(send_tasks).ConfigureAwait(false);
+        }
+
+        public async Task SendAsync(Email Message, Sender From, IEnumerable<Recipient> To)
+        {
+            foreach (var recipient in To)
+                await SendAsync(Message, From, recipient).ConfigureAwait(false);
+        }
     }
 }
